81b3fca35ba6fb3d833378f3fe01d613855bb9e3
Justin Lee <lee.justin001126@gmail.com>
2023-02-10 09:10:54 -0800

    add index.js/index.html content area to handle submissions and writing basic helper functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyApiCall from './services/currency';
 
 // Business Logic
+function getCurrencyData(){
 
+}
+
+function clearResults(){
+  
+}
+
+function handleUserInput(event) {
+  event.preventDefault();
+  clearResults();
+  const userNumber = document.querySelector("#user-input").ariaValueMax;
+  document.querySelector("user-input.value").value = null;
+  getCurrencyData();
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("#user-input-form").addEventListener("submit", handleUserInput);
+});
